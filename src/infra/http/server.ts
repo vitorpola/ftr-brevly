8e56f35649d04a77d25adb1292875a1d250b706d
Vitor Pola <vitorpola@gmail.com>
2025-05-12 18:40:05 -0300

    Commit inicial

---
+++ src/infra/http/server.ts
@@ -0,0 +1,12 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+
+const server = fastify();
+
+server.register(fastifyCors, {
+  origin: "*",
+});
+
+server.listen({ port: 3333 }, () => {
+  console.log("HTTP server running!");
+});
